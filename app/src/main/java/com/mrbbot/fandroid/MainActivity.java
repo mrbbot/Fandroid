f307e89ad0b44c5c32f7c9fc04ff19a8628d4100
MrBBot <me@mrbbot.co.uk>
2016-09-14 15:27:38 +0100

    Added fan animation

--- app/src/main/java/com/mrbbot/fandroid/MainActivity.java
+++ app/src/main/java/com/mrbbot/fandroid/MainActivity.java
@@ -1,15 +1,27 @@
 package com.mrbbot.fandroid;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ObjectAnimator startAnimation, rotateAnimation, stopAnimation;
+    private boolean rotating, changing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +30,54 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageView blades = (ImageView) findViewById(R.id.bladesView);
+
+        startAnimation = ObjectAnimator.ofFloat(blades, "rotation", 0f, 360f);
+        startAnimation.setDuration(1000);
+        startAnimation.setInterpolator(new AccelerateInterpolator(1));
+        startAnimation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                rotateAnimation.start();
+                changing = false;
+            }
+        });
+
+        rotateAnimation = ObjectAnimator.ofFloat(blades, "rotation", 0f, 360f);
+        rotateAnimation.setDuration(500);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setRepeatCount(ObjectAnimator.INFINITE);
+        rotateAnimation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                if(!rotating) {
+                    rotateAnimation.cancel();
+                    stopAnimation.start();
+                }
+            }
+        });
+
+        stopAnimation = ObjectAnimator.ofFloat(blades, "rotation", 0f, 360f);
+        stopAnimation.setDuration(1000);
+        stopAnimation.setInterpolator(new DecelerateInterpolator(1));
+        stopAnimation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                changing = false;
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(!changing) {
+                    rotating = !rotating;
+                    changing = true;
+
+                    if(rotating)
+                        startAnimation.start();
+                }
             }
         });
     }
